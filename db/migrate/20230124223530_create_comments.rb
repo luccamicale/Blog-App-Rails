2b31dbbcc16ba73200aeba9d70c86a90d40ed089
luccamicale <luccamicale2015@gmail.com>
2023-01-24 19:44:45 -0300

    created blog app and migrations

---
+++ db/migrate/20230124223530_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.references :author, foreing_key: {to_table: 'users'}
+      t.references :post, foreing_key: {to_table: 'posts'}
+      t.text :text
+      t.timestamps
+    end
+  end
+end
