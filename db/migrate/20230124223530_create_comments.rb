7310812b76f40c80842472735e96cb1a22cfb95f
luccamicale <luccamicale2015@gmail.com>
2023-01-24 20:25:11 -0300

    add linters

--- db/migrate/20230124223530_create_comments.rb
+++ db/migrate/20230124223530_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, foreing_key: {to_table: 'users'}
-      t.references :post, foreing_key: {to_table: 'posts'}
+      t.references :author, foreing_key: { to_table: 'users' }
+      t.references :post, foreing_key: { to_table: 'posts' }
       t.text :text
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -52,5 +52,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_223543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
