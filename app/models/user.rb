55301120d53aef7accc2e1ce4454fba63afe723d
luccamicale <luccamicale2015@gmail.com>
2023-01-25 17:39:49 -0300

    modified models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :comments, foreing_key: 'author_id'
-    has_many :posts, foreing_key: 'author_id'
-    has_many :likes, foreing_key: 'author_id'
+    has_many :comments, foreing_key: author_id
+    has_many :posts, foreing_key: author_id
+    has_many :likes, foreing_key: author_id
 end
