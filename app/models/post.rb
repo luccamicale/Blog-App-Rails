f9b47d900089ba7758124bd013eed606febc83a6
luccamicale <luccamicale2015@gmail.com>
2023-01-26 00:25:52 -0300

    add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, foreign_key: 'author_id', class_name: 'User'
-    has_many :comments, foreign_key: :post_id
-    has_many :likes, foreign_key: :post_id
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+
+  def update_posts_counter
+    author.increment(:posts_counter)
+  end
+
+  def recent_comments
+    posts.order(created_at: :desc).limit(5)
+  end
 end
